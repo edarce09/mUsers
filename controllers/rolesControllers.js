050823c5970390defebabdfc71c2294278a465fa
edarce09 <edarce09@gmail.com>
2018-01-22 11:16:00 -0500

    better structure for load roles

--- controllers/rolesControllers.js
+++ controllers/rolesControllers.js
@@ -1,11 +1,18 @@
-'use stric'
+'use strict'
 
 let db = require('../../../mongooseSetUp');
 let Role = require('../models/rolesModel');
 let response = require('/home/kirsch/Repository/jsRepos/myNpmDependencies/responses');
+let controllerBase = require('/home/kirsch/Repository/jsRepos/myNpmDependencies/ozControllerBase');
 
 const getAll = function findAllRoles(req, res){
-  let params = {};
+  let populate = 'permissions';
+  let params = {
+    options:{
+      populate: populate,
+      sort: 'name'
+    }
+  }
   Role.loadAll(params, (err, roles)=>{
     if(err) return response.serverError(res, roles, err);
     if(!roles) return response.notFound(res);
@@ -13,12 +20,18 @@ const getAll = function findAllRoles(req, res){
   });
 }
 
+/*
+cons getOne = findOneRole(req, res){
+  let role =  '';  
+}
+*/
+
 const saveRole = function createsNewRole(req, res){
   let role = new Role();
   let params = req.body;
   role.name = req.body.name;
   role.save((err, r)=>{
-    if(err) response.serverError(res, r, err);
+    if(err) return response.serverError(res, r, err);
     return response.created(res, r);
   });
 }
